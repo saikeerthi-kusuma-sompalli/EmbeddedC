df0c43a1d57078205398e9ec51cde2facff4ff40
saikeerthi-kusuma-sompalli <39005873+saikeerthi-kusuma-sompalli@users.noreply.github.com>
2021-05-03 23:27:09 +0530

    Update in activity1

---
+++ src/activity1.c
@@ -0,0 +1,17 @@
+#include"../inc/activity1.h"
+
+#include <avr/io.h>
+#include<util/delay.h>
+void activity1_init(){
+
+    DDRB = DDRB |  (1<<PB0);
+    PORTB = PORTB & ~(1<<PB0);
+
+    //Set D0 as Input and initialize it to high
+    DDRD = DDRD & ~(1<<PD0);
+    PORTD = PORTD |  (1<<PD0);
+
+    //Set D2 as Input and initialize it to high
+    DDRD = DDRD & ~(1<<PD2);
+    PORTD = PORTD |  (1<<PD2);
+}
