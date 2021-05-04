9c687e373cb13a28c84e1e5e7ef92eea76d20c23
saikeerthi-kusuma-sompalli <39005873+saikeerthi-kusuma-sompalli@users.noreply.github.com>
2021-05-04 12:10:35 +0530

    Update activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -6,8 +6,6 @@
 
 #define LED_ON (PORTB |= (1<<PB0))    //LED ON
 #define LED_OFF (PORTB &=~(1<<PB0))   //LED OFF
-#define SENSOR_ON !(PINB&(1<<PD2))     // Switch1 ON
-#define HEATER_ON !(PINB&(1<<PD3))     // Switch2 ON
 
 void activity1_init();
 /**
